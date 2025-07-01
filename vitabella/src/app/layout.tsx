7791173e19f354e061be5ae9dea54fb2b11c8ac4
Ari Bradshaw <ari.bradshaw@vanderbilt.edu>
2025-07-01 11:26:10 -0700

    Google Tag

--- vitabella/src/app/layout.tsx
+++ vitabella/src/app/layout.tsx
@@ -21,7 +21,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TDVW2M77');
+        `}} />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YN2NTS67LL"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-YN2NTS67LL');
+        `}} />
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDVW2M77" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
         <ClarityInit />
         <SpeedInsights /> {/* <-- Add this line to enable Vercel Speed Insights */}
         <Header />
